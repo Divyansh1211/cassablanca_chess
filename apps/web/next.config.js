46009300f0701ae89af957e8ae0e672f905e0b58
divyansh1211 <divyanshmanchanda1211@gmail.com>
2025-03-19 19:22:57 +0530

    updated next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; connect-src 'self' http://api.divyansh.lol ws://ws.divyansh.lol;",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://chess.divyansh.lol",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
