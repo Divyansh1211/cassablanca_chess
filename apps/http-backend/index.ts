976910d718f3e28049f545450881c748004c7a2a
Aditya Singla <adityasingla.2802@gmail.com>
2025-03-19 21:36:02 +0530

    fix null origin issue

--- apps/http-backend/index.ts
+++ apps/http-backend/index.ts
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: function (origin, callback) {
       if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, origin);
+        callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
       }
