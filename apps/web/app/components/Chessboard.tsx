1f0406ccb333c7c0a677ffb72609a8aa3a3386b5
divyansh1211 <divyanshmanchanda1211@gmail.com>
2025-03-20 00:09:24 +0530

    cors O_O

--- apps/web/app/components/Chessboard.tsx
+++ apps/web/app/components/Chessboard.tsx
@@ -9,7 +9,9 @@ import { MoveTable } from "./MoveTable";
 import { getRandomGame, IGameData, moveFormatter } from "../helper";
 import { redirect } from "next/navigation";
 
-let socket: Socket = io("http://localhost:3001");
+let socket: Socket = io("http://localhost:3001", {
+  path: "/",
+});
 
 export const ChessboardComponent = () => {
   let [game, setGame] = useState<Chess>();
